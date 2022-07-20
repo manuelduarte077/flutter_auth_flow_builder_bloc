3ba198d243a5799530dfb616e1bc0d314612ea94
@manuelduarte077 <carlosd99m@gmail.com>
2022-07-19 23:07:49 -0600

    chore: initial commit

---
+++ android/app/src/main/kotlin/com/getnerdify/flutter_auth_flow_builder_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.getnerdify.flutter_auth_flow_builder_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth_flow_builder_bloc/app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
