2ab65149849951fa736e3b431dc2843f75c9b612
@manuelduarte077 <carlosd99m@gmail.com>
2022-07-20 23:56:43 -0600

    flow builder to redirect the user on the pre-defined screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_auth_flow_builder_bloc/app/app.dart';
 import 'package:flutter_auth_flow_builder_bloc/bloc_observer.dart';
 import 'package:flutter_auth_flow_builder_bloc/firebase_options.dart';
+import 'package:flutter_auth_flow_builder_bloc/repositories/auth_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
   return BlocOverrides.runZoned(
     () async {
-      runApp(const MyApp());
+      WidgetsFlutterBinding.ensureInitialized();
+
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+      final authRepository = AuthRepository();
+
+      runApp(MyApp(authRepository: authRepository));
     },
     blocObserver: AppBlocObserver(),
   );
